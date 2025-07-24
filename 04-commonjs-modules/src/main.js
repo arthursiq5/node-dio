39e19572c3a8f15b69e544986e1da0c63d69000f
arthursiq5 <arthur.siq5@gmail.com>
2025-07-24 00:19:08 -0300

    Adicionado exemplo de export de constantes

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,9 +1,14 @@
 const p = require('./services/products')
+const config = require('./services/config')
 
 async function main() {
     console.log(await p.getFullName(123, 'teste'))
     console.log(await p.getFullName(456, 'teste'))
     console.log('Olá mundo');
+
+    console.log('----------------------')
+
+    console.log(config)
 }
 
 main();
