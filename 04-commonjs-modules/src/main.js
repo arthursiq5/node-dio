4cfa716a44837d7d7ef9f87fd1b318f2158d568a
arthursiq5 <arthur.siq5@gmail.com>
2025-07-26 14:59:28 -0300

    Corrigidos módulos

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,7 +1,9 @@
 const p = require('./services/products')
 const config = require('./services/config')
+const { connectToDatabase, disconnectDatabase } = require('./services/database')
 
 async function main() {
+    connectToDatabase('teste')
     console.log(await p.getFullName(123, 'teste'))
     console.log(await p.getFullName(456, 'teste'))
     console.log('Olá mundo');
@@ -9,6 +11,7 @@ async function main() {
     console.log('----------------------')
 
     console.log(config)
+    disconnectDatabase()
 }
 
 main();
---
+++ 04-commonjs-modules/src/services/database.js
@@ -0,0 +1,7 @@
+exports.connectToDatabase = (dataName) => {
+    console.log(`Se conectando ao banco: ${dataName}`)
+}
+
+exports.disconnectDatabase = () => {
+    console.log('Desconectando...')
+}
