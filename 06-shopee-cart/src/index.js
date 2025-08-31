b9aeddfd72223435f7a134e5f044f3be7fe8c2b6
arthursiq5 <arthur.siq5@gmail.com>
2025-08-31 15:11:45 -0300

    Cálculo do total

--- 06-shopee-cart/src/index.js
+++ 06-shopee-cart/src/index.js
@@ -1,3 +1,4 @@
+import { addItem, calculateTotal } from "./services/cart.js";
 import createItem from "./services/item.js";
 
 const cart = []
@@ -7,4 +8,7 @@ console.log('hello shopee');
 const item1 = await createItem('hotwheels ferrari', 20.99, 1)
 const item2 = await createItem('hotwheels lamborghini', 39.99, 3)
 
-console.log(item2.subtotal());
+await addItem(cart, item1)
+await addItem(cart, item2)
+
+console.log(await calculateTotal(cart));
--- 06-shopee-cart/src/services/cart.js
+++ 06-shopee-cart/src/services/cart.js
@@ -1,15 +1,15 @@
-async function addItem(userCart) {
+export async function addItem(userCart, item) {
+    userCart.push(item)
+}
+
+export async function deleteItem(userCart, name) {
 
 }
 
-async function deleteItem(userCart, name) {
+export async function removeItem(userCart, index) {
 
 }
 
-async function removeItem(userCart, index) {
-
-}
-
-async function calculateTotal(userCart) {
-    
+export async function calculateTotal(userCart) {
+    return userCart.reduce((total, item) => total + item.subtotal(), 0)
 }
