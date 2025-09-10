d983b91853a53260e341b92b25c169577b1aca37
arthursiq5 <arthur.siq5@gmail.com>
2025-09-09 23:23:49 -0300

    Exemplo de export de constante

--- 05-esm-modules/src/index.js
+++ 05-esm-modules/src/index.js
@@ -1,4 +1,6 @@
+import { getDataFromApi } from "./utils/api.js";
 import { connectToDatabase, disconnectDatabase } from "./utils/database.js";
 
 await connectToDatabase('teste')
+await getDataFromApi()
 await disconnectDatabase()
---
+++ 05-esm-modules/src/utils/api.js
@@ -0,0 +1,8 @@
+export const key = {
+    value: 'gfgj0mjjt54gfs',
+    permission: 'admin'
+}
+
+export const getDataFromApi = async () => {
+    console.log('Conectado à API...');
+}
